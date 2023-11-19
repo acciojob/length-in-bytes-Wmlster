b1aedb20b66a6e8bfbc19a3087ab35e09f099fcd
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-19 16:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const byteSize = (str) => {
   // write your code here
-};
+	
+	const byteSize =  new Blob([str]).size;
+
+
+	return byteSize;  
+
+}; 
 
 // Do not change the code below
 const str = prompt("Enter some string.");
